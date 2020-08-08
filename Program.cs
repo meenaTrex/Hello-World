a005334665edd4b876da8dd60532988a095f53b0
meenakshithukral <meenakshi.thukral@nagarro.com>
2020-08-08 18:01:50 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi this is NAGP Devops Hello World App!");
+        }
+    }
+}
